ed1b104a4785fc8b9c9b68f504d32ca836504d26
Андрей черенецкий <you@example.com>
2019-06-05 20:41:45 +0300

    site

---
+++ gulp master/dist/assets/js/app.js
@@ -0,0 +1,35 @@
+$(function () {
+
+  var header = $("#Header"),
+    introH = $("#Intro").innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+
+  /* Fixed Header */
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function() {
+    scrollOffset = $(this).scrollTop();
+
+    checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+    if( scrollOffset >= introH ) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+
+  /* Menu nav toggle */
+  $("#nav_toggle").on("click", function(event) {
+    event.preventDefault();
+
+    $(this).toggleClass("active");
+    $("#Nav").toggleClass("active");
+  });
+
+
+});
