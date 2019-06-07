bea1188a50f1e7d8d14fa059a28c693ac1563313
Андрей черенецкий <you@example.com>
2019-06-07 15:13:56 +0300

    acc

--- gulp master/dist/assets/js/app.js
+++ gulp master/dist/assets/js/app.js
@@ -68,3 +68,14 @@
    });
 
 });
+
+ const acc = document.getElementsByClassName("Accordion__button");
+let i;
+for(i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    panel.style.display == 'block' ?  panel.style.display ='none' :  panel.style.display = 'block';
+
+  });
+}
