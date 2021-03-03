3574502773391e35e9a6c296feeed06f74c0ed4f
Andru <rodomir3119841985@gmail.com>
2021-03-03 09:42:34 +0300

    new site

---
+++ javascript/js.js
@@ -0,0 +1,39 @@
+const accButton = document.querySelectorAll('.accordion__button');
+  accButton.forEach(btn => btn.addEventListener('click',()=>{
+    const openButton = btn.nextElementSibling.classList.contains('accordion__panel--open');
+    if(openButton) {
+      closeAcc(btn.nextElementSibling);
+    } else {
+      accButton.forEach(x=>closeAcc(x.nextElementSibling));
+      openAcc(btn.nextElementSibling);
+    }
+  }))
+
+  function openAcc(item) {
+    item.classList.add('accordion__panel--open');
+    item.style.maxHeight = item.scrollHeight+'px';
+    item.style.padding = '1rem auto'
+  }
+
+  function closeAcc(item) {
+    item.classList.remove('accordion__panel--open');
+    item.style.maxHeight = '0';
+    item.style.padding = '0 1.5rem 0 1.5rem';
+  }
+
+
+  const menu = document.querySelector('.nav');
+  const burger = document.querySelector('.burger');
+  const overlay = document.querySelector('.overlay');
+
+  burger.addEventListener('click', ()=>{
+    menu.classList.add('open');
+    overlay.classList.add('open');
+  
+  });
+
+  overlay.addEventListener('click',()=>{
+    menu.classList.remove('open');
+    overlay.classList.remove('open');
+   
+  })
